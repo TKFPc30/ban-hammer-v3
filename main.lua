3436060d0828a445195443c30961b64696121203
TKFPc30 <aisyrapid@gmail.com>
2025-07-26 22:20:13 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
-WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
 ]]
 	game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("-gh 9350274205 12103236257 2309346267 48474313 62724852 451220849 48474294")
 
